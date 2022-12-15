1135f5aa72278fb656bd6d97cbb146be2b77fb17
Nahum Añez <nahumatencio@gmail.com>
2022-12-14 22:23:47 -0500

    deploying my App

---
+++ src/data/tasks.js
@@ -0,0 +1,19 @@
+export const tasks = [
+  {
+    id: 0,
+    title: "mi primera tarea",
+    description: "Mi primera tarea",
+  },
+
+  {
+    id: 1,
+    title: "mi segunda tarea",
+    description: "Mi segunda tarea",
+  },
+
+  {
+    id: 2,
+    title: "mi tercera tarea",
+    description: "Mi tercera tarea",
+  },
+];
